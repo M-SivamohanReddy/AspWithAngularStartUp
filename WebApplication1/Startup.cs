97774fd252fcfee5407958a7315b3749eb4f1a1a
SIVAMOHAN REDDY <sivamohan.reddy@walkingtree.tech>
2018-12-07 12:51:30 +0530

    Initial Commit (Creating the database with tables with EFCore Code First).
    Commands needed to create and update migrations to the database..
    cmd 1> dotnet ef database update
    cmd 2> dotnet ef migrations add MigrationName
    cmd 3> dotnet ef database update

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -3,23 +3,30 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           // Telling which database we are using
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DutchConnectionString"));
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
